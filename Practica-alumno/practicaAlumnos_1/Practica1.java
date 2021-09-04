8a7502d9549d73abde82c29765060516b85f7a40
Beto <betoml5@hotmail.com>
2021-09-04 18:36:50 -0500

    First step work

--- Practica-alumno/practicaAlumnos_1/Practica1.java
+++ Practica-alumno/practicaAlumnos_1/Practica1.java
@@ -8,6 +8,8 @@ public class Practica1 {
         Alumno[] arrayAlumnos = new Alumno[10];
         Scanner input = new Scanner(System.in);
 
+        arrayAlumnos[0] = new Alumno("Beto", 2, 10, 10);
+
         int option;
         do {
             System.out.println("- - - MENU DE OPCIONES - - -");
@@ -24,18 +26,22 @@ public class Practica1 {
                     System.out.println("POSICIONES: " + Arrays.toString(arrayAlumnos));
                     System.out.println("Ingresa la posicion del nuevo alumno: ");
                     int position = input.nextInt();
-                    if (arrayAlumnos[position] != null) {
-                        System.out.println("Ingresa el nombre del alumno");
-                        String nombre = input.next();
-                        System.out.println("Ingresa el grupo del alumno");
-                        int grupo = input.nextInt();
-                        System.out.println("Ingresa la calificacion 1");
-                        float cal_1 = input.nextFloat();
-                        System.out.println("Ingresa la calificacion 2");
-                        float cal_2 = input.nextFloat();
-                        arrayAlumnos[position] = new Alumno(nombre, grupo, cal_1, cal_2);
+
+                    while (arrayAlumnos[position] != null) {
+                        System.out.println("POSICIONES: " + Arrays.toString(arrayAlumnos));
+                        System.out.println("Ingresa la posicion del nuevo alumno: ");
+                        position = input.nextInt();
                     }
 
+                    System.out.println("Ingresa el nombre del alumno");
+                    String nombre = input.next();
+                    System.out.println("Ingresa el grupo del alumno");
+                    int grupo = input.nextInt();
+                    System.out.println("Ingresa la calificacion 1");
+                    float cal_1 = input.nextFloat();
+                    System.out.println("Ingresa la calificacion 2");
+                    float cal_2 = input.nextFloat();
+                    arrayAlumnos[position] = new Alumno(nombre, grupo, cal_1, cal_2);
                     break;
 
                 default:
