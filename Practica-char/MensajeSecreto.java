d1ca828cf2a47d2b3c0f37df43f065d48c719d6f
Beto <betoml5@hotmail.com>
2021-09-30 11:46:48 -0500

    Insertar datos en 3 pilas do

--- Practica-char/MensajeSecreto.java
+++ Practica-char/MensajeSecreto.java
@@ -13,31 +13,42 @@ public class MensajeSecreto {
         PilaChar pilaChar2 = new PilaChar(palabra.length());
         PilaChar pilaChar3 = new PilaChar(palabra.length());
 
-        for (int i = 0; i < palabra.length() - 2; i = pilaChar1.numElementos() + pilaChar2.numElementos()
-                + pilaChar3.numElementos()) {
-            pilaChar1.insertar(palabra.charAt(i));
-            pilaChar2.insertar(palabra.charAt(i + 1));
-            pilaChar3.insertar(palabra.charAt(i + 2));
+        // int index = palabra.length();
+        // int aux = 0;
+        // while (index != 0) {
+        // pilaChar1.insertar(palabra.charAt(index));
+        // pilaChar2.insertar(palabra.charAt(index + 1));
+        // pilaChar3.insertar(palabra.charAt(index + 2));
+        // index = index - (pilaChar1.numElementos() + pilaChar2.numElementos() +
+        // pilaChar3.numElementos());
+        // }
+        // int aux = 0;
+        // for (int i = 0; i < palabra.length() - 2; i = pilaChar1.numElementos() +
+        // pilaChar2.numElementos()
+        // + pilaChar3.numElementos()) {
+        // pilaChar1.insertar(palabra.charAt(i));
+        // pilaChar2.insertar(palabra.charAt(i + 1));
+        // pilaChar3.insertar(palabra.charAt(i + 2));
+        // aux = pilaChar1.numElementos() + pilaChar2.numElementos()
+        // + pilaChar3.numElementos();
+        // }
+
+        for (int i = 0; i < palabra.length(); i++) {
+            int resto = i % 3;
+            if (resto == 0) {
+                pilaChar1.insertar(palabra.charAt(i));
+            } else if (resto == 1) {
+                pilaChar2.insertar(palabra.charAt(i));
+            } else {
+                pilaChar3.insertar(palabra.charAt(i));
+            }
         }
+
+        
 
         System.out.println(Arrays.toString(pilaChar1.getP()));
         System.out.println(Arrays.toString(pilaChar2.getP()));
         System.out.println(Arrays.toString(pilaChar3.getP()));
-
-        char auxArray[] = new char[pilaChar1.numElementos() + pilaChar2.numElementos() + pilaChar3.numElementos()];
-
-        for (int i = pilaChar1.getTope(); i >= 0; i++) {
-            auxArray[i] = pilaChar1.eliminar();
-            auxArray[i + 1] = pilaChar1.eliminar();
-        }
-
-        System.out.println(Arrays.toString(pilaChar1.getP()));
-        System.out.println(Arrays.toString(pilaChar2.getP()));
-        System.out.println(Arrays.toString(pilaChar3.getP()));
-
-        // System.out.println(pilaChar1.toString());
-        // System.out.println(pilaChar2.toString());
-        // System.out.println(pilaChar3.toString());
 
     }
 }
