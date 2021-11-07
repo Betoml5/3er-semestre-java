57df9030010c6c1eb1e58995cbc55ec666652db2
Betoml5 <betoml5@hotmail.com>
2021-11-07 11:27:40 -0600

    Finish listaDoctores

--- ListasEnlazadas/Practica-1/ListaDoctores.java
+++ ListasEnlazadas/Practica-1/ListaDoctores.java
@@ -1,8 +1,9 @@
+
 public class ListaDoctores {
     private NodoDoctor inicio;
     private int numNodos;
 
-    // Constructor para crear una lista vacía
+    // Constructor para crear una lista vac�a
     public ListaDoctores() {
         inicio = null;
         numNodos = 0;
@@ -19,7 +20,15 @@ public class ListaDoctores {
 
     // faltan los set..
 
-    // Método que devuelve true si la lista está vacía
+    public void setInicio(NodoDoctor inicio) {
+        this.inicio = inicio;
+    }
+
+    public void setNumNodos(int numNodos) {
+        this.numNodos = numNodos;
+    }
+
+    // M�todo que devuelve true si la lista est� vac�a
     public boolean listaVacia() {
         if (inicio == null)
             return true;
@@ -27,14 +36,38 @@ public class ListaDoctores {
             return false;
     }
 
-    // Método que inserta un nodo al inicio de la lista
+    // M�todo que inserta un nodo al inicio de la lista
     public void insertaInicio(NodoDoctor nuevo) {
         nuevo.setEnlace(inicio);
         inicio = nuevo;
         numNodos++;
     }
 
-    // Método que recorre la lista y despliega en pantalla los datos de los nodos
+    // M�todo que inserta un nodo al final de la lista
+    public void insertaFinal(NodoDoctor x) {
+        NodoDoctor r = inicio;
+        if (listaVacia())
+            inicio = x;
+        else {
+            while (r.getEnlace() != null) {
+                r = r.getEnlace();
+            }
+            r.setEnlace(x);
+        }
+        numNodos++;
+    }
+
+    public NodoDoctor eliminaInicio() {
+        NodoDoctor eliminado = inicio;
+        if (!listaVacia()) {
+            inicio = inicio.getEnlace();
+            eliminado.setEnlace(null);
+            numNodos--;
+        }
+        return eliminado;
+    }
+
+    // M�todo que recorre la lista y despliega en pantalla los datos de los nodos
     public void recorreLista() {
         NodoDoctor r = inicio;
         System.out.print("Empieza la lista:\n");
@@ -61,15 +94,88 @@ public class ListaDoctores {
         }
     }
 
+    public String medicoEspecialidad(String esp) {
+
+        NodoDoctor r = inicio;
+        String s = "Especialidad: " + esp + "\n";
+        while (r != null) {
+            if (r.getDoctor().getEspecialidad().equals(esp)) {
+                s += r.getDoctor().getNombre() + "\n";
+            }
+            r = r.getEnlace(); // avanzar al sig. nodo
+        }
+        return s;
+    }
+
+    public float sumaSueldos() {
+        NodoDoctor r = inicio;
+        float sumaTotal = 0;
+        if (!listaVacia()) {
+            while (r != null) {
+                sumaTotal += r.getDoctor().getSueldo();
+                r = r.getEnlace();
+            }
+
+            return sumaTotal;
+        }
+
+        return sumaTotal;
+    }
+
+    public NodoDoctor eliminarPorNombre(String nombre) {
+        NodoDoctor nodoActual = inicio;
+        NodoDoctor nodoAtras = null;
+
+        if (!listaVacia()) {
+
+            if (numNodos == 1 && nodoActual.getDoctor().getNombre().equals(nombre)) {
+                System.out.println("El doctor " + nodoActual.getDoctor().getNombre() + " ha sido eliminado");
+                return nodoActual = eliminaInicio();
+            }
+
+            if (nodoActual.getDoctor().getNombre().equals(nombre)) {
+                nodoAtras = inicio;
+                nodoActual = inicio.getEnlace();
+                NodoDoctor aux = nodoActual;
+                inicio = null;
+                numNodos--;
+                System.out.println("El doctor " + aux.getDoctor().getNombre() + " ha sido eliminado");
+                return aux;
+            }
+
+            nodoAtras = inicio;
+            nodoActual = inicio.getEnlace();
+            while (nodoActual != null) {
+                if (nodoActual.getDoctor().getNombre().equals(nombre)) {
+                    NodoDoctor aux = nodoActual;
+                    nodoActual = null;
+                    nodoAtras.setEnlace(aux.getEnlace());
+                    numNodos--;
+                    System.out.println("El doctor " + aux.getDoctor().getNombre() + " ha sido eliminado");
+                    return aux;
+                }
+                // Avanzamos en caso de que no se cumpla la condicion.
+                nodoAtras = nodoAtras.getEnlace();
+                nodoActual = nodoActual.getEnlace();
+
+            }
+        }
+        System.out.println("No hemos encontrado ningun doctor con ese nombre");
+        return null;
+    }
+
     // Metodo toString
     public String toString() {
         NodoDoctor r = inicio;
         String s = "";
-        while (r != null) {
-            s += r.toString() + "\n";
-            r = r.getEnlace(); // avanzar al sig. nodo
+        if (!listaVacia()) {
+            while (r != null) {
+                s += r.toString() + "\n";
+                r = r.getEnlace(); // avanzar al sig. nodo
+            }
+            return s;
         }
-        return s;
+        return "No hay elementos en la lista";
     }
 
 }
--- ListasEnlazadas/Practica-1/U4_Practica1.java
+++ ListasEnlazadas/Practica-1/U4_Practica1.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class U4_Practica1 {
@@ -16,30 +17,61 @@ public class U4_Practica1 {
             System.out.println("SELECCIONA CON LOS NUMEROS");
             System.out.println("1.- Dar de dalto a un medico en el turno 1");
             System.out.println("2.- Dar de dalto a un medico en el turno 2");
-            System.out.println("3.- Mostrar médicos del 1er. turno");
-            System.out.println("4.- Mostrar médicos del 2do. turno");
+            System.out.println("3.- Mostrar medicos del 1er. turno");
+            System.out.println("4.- Mostrar medicos del 2do. turno");
             System.out.println("5.- Buscar un doctor");
-            System.out.println("6.- Lista de médicos por especialidad y por turno");
-            System.out.println("7.- Salir");
-            opcion = entrada.nextInt();
+            System.out.println("6.- Lista de medicos por especialidad y por turno");
+            System.out.println("7.- Suma de salarios de los 2 turnos");
+            System.out.println("8.- Eliminar doctor por nombre");
+            System.out.println("9.- Salir");
 
+            opcion = entrada.nextInt();
+            boolean correcto = true;
             switch (opcion) {
             case 1:
                 System.out.println("Ingresa el nombre del doctor");
                 entrada.nextLine();
                 nombre = entrada.nextLine();
                 System.out.println("Ingresa la especialidad del doctor");
+                System.out.println("--Especialidad a elegir--");
+                System.out.println("*Cardiologia");
+                System.out.println("*Pediatria");
+                System.out.println("*Dermatologia");
                 especialidad = entrada.nextLine();
+                correcto = true;
+                do {
+                    if (especialidad.equals("Cardiologia") || especialidad.equals("Pediatria")
+                            || especialidad.equals("Dermatologia")) {
+                        correcto = false;
+                    } else {
+                        System.out.println("Esa especialidad no esta en las opciones, ingresa otra");
+                        especialidad = entrada.nextLine();
+                    }
+                } while (correcto);
                 System.out.println("Ingresa el sueldo del doctor");
                 sueldo = entrada.nextFloat();
-                turno1.insertaInicio(new NodoDoctor(new Doctor(nombre, especialidad, sueldo)));
+                turno1.insertaFinal(new NodoDoctor(new Doctor(nombre, especialidad, sueldo)));
                 break;
             case 2:
                 System.out.println("Ingresa el nombre del doctor");
                 entrada.nextLine();
                 nombre = entrada.nextLine();
                 System.out.println("Ingresa la especialidad del doctor");
+                System.out.println("--Especialidad a elegir--");
+                System.out.println("*Cardiologia");
+                System.out.println("*Pediatria");
+                System.out.println("*Dermatologia");
                 especialidad = entrada.nextLine();
+                correcto = true;
+                do {
+                    if (especialidad.equals("Cardiologia") || especialidad.equals("Pediatroa")
+                            || especialidad.equals("Dermatologia")) {
+                        correcto = false;
+                    } else {
+                        System.out.println("Esa especialidad no est� en las opciones, ingresa otra");
+                        especialidad = entrada.nextLine();
+                    }
+                } while (correcto);
                 System.out.println("Ingresa el sueldo del doctor");
                 sueldo = entrada.nextFloat();
                 turno2.insertaInicio(new NodoDoctor(new Doctor(nombre, especialidad, sueldo)));
@@ -56,21 +88,71 @@ public class U4_Practica1 {
                 nombre = entrada.nextLine();
                 System.out.println(turno1.buscarDoctor(nombre) + " TURNO 1");
                 System.out.println(turno2.buscarDoctor(nombre) + " TURNO 2");
-
                 break;
             case 6:
-                break;
+                System.out.println("Ingresa el n�mero de especialidad que deseas colsultar");
+                System.out.println("--Especialidad a elegir--");
+                System.out.println("*Cardiologia");
+                System.out.println("*Pediatria");
+                System.out.println("*Dermatologia");
+                String esp = entrada.nextLine();
+                correcto = true;
+                do {
+                    if (esp.equals("Cardiolog�a") || esp.equals("Pediatr�a") || esp.equals("Dermatolog�a")) {
+                        correcto = false;
+                    } else {
+                        System.out.println("Esa especialidad no est� en las opciones, ingresa otra");
+                        esp = entrada.nextLine();
+                    }
+                } while (correcto);
 
+                int t = 0;
+                do {
+                    System.out.println("Ingresa el n�mero de turno que deseas consultar");
+                    t = entrada.nextInt();
+                } while (t <= 0 || t >= 3);
+                switch (t) {
+                case 1:
+                    if (turno1.listaVacia()) {
+                        System.out.println("NO HAY DATOS EN EL TURNO 1");
+                    } else {
+                        System.out.println("TURNO 1");
+                        System.out.println(turno1.medicoEspecialidad(esp));
+                    }
+                    break;
+                case 2:
+                    if (turno2.listaVacia()) {
+                        System.out.println("NO HAY DATOS EN EL TURNO 2");
+                    } else {
+                        System.out.println("TURNO 2");
+                        System.out.println(turno2.medicoEspecialidad(esp));
+                    }
+                    break;
+                default:
+
+                    break;
+                }
+                break;
             case 7:
-                opcion = 7;
+                System.out.println(turno1.sumaSueldos());
                 break;
 
+            case 8:
+                System.out.println("Ingresa el nombre del doctor");
+                entrada.nextLine();
+                nombre = entrada.nextLine();
+                turno1.eliminarPorNombre(nombre);
+
+                break;
+            case 9:
+                opcion = 9;
+                break;
             default:
-                System.out.println("Esa opcion no existe");
+
                 break;
             }
 
-        } while (opcion != 7);
+        } while (opcion != 9);
 
     }
 }
